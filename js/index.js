6c4496213b8dbe51df7cb5236bff5bcbad9b5277
jandonggun <donggontrannguyen@gmail.com>
2021-03-14 16:39:45 +0700

    add guide page

--- js/index.js
+++ js/index.js
@@ -8,6 +8,127 @@ window.addEventListener("DOMContentLoaded", function () {
   var menu = document.querySelector(".menu");
   var scrollTop = document.querySelector(".scrollTop");
   var btn = document.querySelector(".btn");
+  const copyGuide = document.querySelector(".guide__btn");
+  const copyGuideCode = document.querySelector(".guide__btn-code");
+  const inputGuide = document.querySelector(".guide__input");
+  const code = document.querySelector("#code");
+  code.value = `
+  function doGet() {
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const ws = ss.getSheetByName("vocabulary");
+    const data = ws.getRange("A1").getDataRegion().getValues();
+    const header = data.shift();
+  
+    const jsonArray = data.map((r) => {
+      let obj = {};
+      header.forEach((h, i) => {
+        obj[h] = r[i];
+      });
+      return obj;
+    });
+  
+    const response = [{ status: 200, data: jsonArray }];
+    return sendJSON_(response);
+  }
+  
+  function doPost(e) {
+    const type = e.parameter.type;
+    const value = e.parameter.value;
+    if (type === "delete") {
+      deleteRow(value);
+    }
+    if (type === "edit") {
+      const arr = value.split(":");
+      replaceRow(arr);
+    }
+  
+    let jsonResponse;
+    const requiredColumns = ["Terms", "Category","VietnameseMeaning"];
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const ws = ss.getSheetByName("vocabulary");
+    const data = ws.getRange(2, 1, ws.getLastRow(), ws.getLastColumn());
+    const headers = ws.getRange(1, 1, 1, ws.getLastColumn()).getValues()[0];
+    const headersOriginalOrder = headers.slice();
+  
+    // remove id columns header
+    headers.sort();
+  
+    const body = e.postData.contents;
+    const bodyJSON = JSON.parse(body);
+    const headersPassed = Object.keys(bodyJSON).sort();
+  
+    if (!checkColumnsPassed_(headers, headersPassed, requiredColumns)) {
+      jsonResponse = { status: 500, message: "Invalid Arguments Passed" };
+      return sendJSON_(jsonResponse);
+    }
+  
+    const arrayOfData = headersOriginalOrder.map((h) => bodyJSON[h]);
+    ws.appendRow(arrayOfData);
+    data.sort(1);
+    return sendJSON_(bodyJSON);
+  }
+  
+  function replaceRow(value) {
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const editSheet = ss.getSheetByName("vocabulary");
+    const lastRowEdit = editSheet.getLastRow();
+    for (let i = 2; i <= lastRowEdit; i++) {
+      if (editSheet.getRange(i, 1).getValue() === value[0]) {
+        editSheet.getRange("A" + i + ":C" + i).setValues([value]);
+      }
+    }
+  }
+  
+  function deleteRow(value) {
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const editSheet = ss.getSheetByName("vocabulary");
+    const lastRowEdit = editSheet.getLastRow();
+    for (let i = 2; i <= lastRowEdit; i++) {
+      if (editSheet.getRange(i, 1).getValue() === value) {
+        editSheet.deleteRow(i);
+      }
+    }
+  }
+  
+  
+  function checkColumnsPassed_(
+    arrAllColumns,
+    arrColumnsPassed,
+    arrRequiredColumns
+  ) {
+    if (!arrRequiredColumns.every((item) => arrColumnsPassed.includes(item)))
+      return false;
+    if (!arrColumnsPassed.every((item) => arrAllColumns.includes(item)))
+      return false;
+    return true;
+  }
+  
+  function sendJSON_(response) {
+    return ContentService.createTextOutput(JSON.stringify(response)).setMimeType(
+      ContentService.MimeType.JSON
+    );
+  }
+  `;
+
+  copyGuide.addEventListener("click", (e) => {
+    inputGuide.select();
+    document.execCommand("copy");
+    copyGuide.textContent = "Copied!";
+
+    setTimeout(() => {
+      copyGuide.textContent = "Copy";
+    }, 2000);
+  });
+
+  copyGuideCode.addEventListener("click", (e) => {
+    code.select();
+    document.execCommand("copy");
+    copyGuideCode.textContent = "Copied!";
+
+    setTimeout(() => {
+      copyGuideCode.textContent = "Copy";
+    }, 2000);
+  });
 
   window.addEventListener("scroll", function () {
     var offset = window.pageYOffset;
